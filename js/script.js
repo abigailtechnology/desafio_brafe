bc61623f602bf8b2a30b67c7d937b953b0ddd9fe
abigailtechnology <116679728+abigailtechnology@users.noreply.github.com>
2023-08-16 16:50:10 -0300

    7 Version

---
+++ js/script.js
@@ -0,0 +1,33 @@
+/*document.getElementById("enviar").onclick = function(){enviarEmail()};
+  function enviarEmail() {
+    window.alert("E-mail enviado!")
+  }*/
+
+
+document.addEventListener("DOMContentLoaded", function () {
+
+  const btnEmail = document.getElementById("btn-email");
+  const enviar = document.getElementById("enviar");
+  const confirm = document.getElementById("confirm");
+  const enviado = document.getElementById("enviado");
+
+  enviar.addEventListener("click", function (event) {
+    event.preventDefault();
+    enviarEmail();
+  });
+
+
+  function enviarEmail() {
+    const email = btnEmail.value;
+
+    setTimeout(function() {
+      confirm.style.display = "block";
+      enviado.textContent = email;
+      btnEmail.value = "";
+
+      setTimeout(function() {
+        confirm.style.display = "none";
+      }, 1000);
+    }, 1500);
+  }
+});
